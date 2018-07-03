3c60bd1f15ad0639d188a67e009281d6f41f570d
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2018-07-04 09:26:05 +1000

    removed view

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,8 @@ import * as express from "express";
 // Routes
 import {index} from "./routes/index";
 
-
-// Create Express server
 export const app = express();
 
-// Express configuration
 app.set("port", process.env.PORT || 3000);
-
 app.use("/", index);
+
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import * as controller from "../welcome/index";
+import * as welcome from "../welcome/index";
 
 export const index = Router();
 
-index.get("/", controller.index);
+index.get("/", welcome.index);
