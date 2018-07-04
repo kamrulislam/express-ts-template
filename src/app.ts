1fa0206dfea9189536fb985174f3781280b998a1
gotaheads <gotahiroki@gmail.com>
2018-07-04 10:10:34 +1000

    ts lint fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
 import * as pkg from '../package.json';
-export const apiVersion = pkg.version;
+export const apiVersion: string = pkg.version;
 
-import * as express from "express";
+import * as express from 'express';
 // Routes
-import {index} from "./routes/index";
+import {index} from './routes/index';
 
 export const app = express();
 
-app.set("port", process.env.PORT || 3000);
-app.use("/", index);
-
+app.set('port', process.env.PORT || 3000);
+app.use('/', index);
