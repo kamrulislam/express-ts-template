317cebc06e9b887d125833834253ed1d7b9d0912
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2018-09-07 14:14:28 +1000

    fail if ip not retrieved

--- scripts/host-ip.js
+++ scripts/host-ip.js
@@ -1,19 +1,25 @@
 const R = require('ramda');
+const {isNil, isEmpty, either} = R;
+const isNilOrEmpty = either(isNil, isEmpty);
 const program = require('commander');
 program
   .version('1.0.0')
-  .option('-c, --container-name', 'The name of docker container')
+  .option('-c, --container-name [containerName]', 'The name of docker container')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
+console.log('host-ip containerName: %s', program.containerName);
+
 if (R.isNil(program.containerName)) {
   throw new Error('You must provide a container name to find the ip host address');
 }
 
 const shell = require('shelljs');
+
 const ip = shell.exec(`docker exec ${program.containerName} /sbin/ip route|awk '/default/ { print $3 }'`, {silent:true}).stdout;
 
-if (R.isNil(ip)) {
+console.log('host-ip ip: %s', ip);
+
+if (isNilOrEmpty(ip)) {
   throw new Error(`Unable to find the ip address of ${program.containerName}`);
 }
 
